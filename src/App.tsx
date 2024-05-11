3b0ad64de66c90414adb9e6d1ea1632f79f20d6b
Andrei <roehm.andrei.zacarias@gmail.com>
2024-05-11 20:48:55 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import SignIn from "./SignIn"
+
+function App() {
+
+  return (
+  <div>
+    <div className="flex h-screen flex-col items-center justify-between p-24">      
+      <SignIn/>
+    </div>
+  </div>
+)
+ 
+   
+}
+
+export default App;
